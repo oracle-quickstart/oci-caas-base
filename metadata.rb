61d8a9d481493f4ebcfa81003fb9238b4fad14ff
Jon DeCamp <jjdecamp@macbook-pro.lan>
2020-12-09 14:28:36 -0800

    metadata bump

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Base cookbook for OCI CAAS'
 version '0.1.2'
 chef_version '>= 16.0'
 
-depends 'wazuh_agent'
+#depends 'wazuh_agent'
 depends 'clamav', '~> 1.3.0'
 depends 'yum', '~> 3.13.0'
 depends 'wazuh_agent', '~> 0.1.0'
