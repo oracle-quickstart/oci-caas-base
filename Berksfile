f00c4165d6f1b459ba783ac45824fc9f08f98f8c
Jon DeCamp <jjdecamp@macbook-pro.lan>
2020-12-10 10:35:02 -0800

    0.2.0 for base

--- Berksfile
+++ Berksfile
@@ -3,5 +3,4 @@ source 'https://supermarket.chef.io'
 metadata
 
 cookbook 'clamav', '~> 1.3.0'
-# cookbook 'yum-epel', '~> 3.3.0'
 cookbook 'wazuh_agent', git: 'https://github.com/wazuh/wazuh-chef.git',rel: 'cookbooks/wazuh_agent'
--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,15 @@
 name 'oci_caas_base'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
-license 'The Universal Permissive License (UPL), Version 1.0'
+license 'Universal Permissive License v1.0'
 description 'Base cookbook for OCI CAAS'
-version '0.1.3'
+version '0.2.0'
 chef_version '>= 16.0'
 
 depends 'wazuh_agent'
 depends 'clamav', '~> 1.3.0'
 depends 'yum', '~> 3.13.0'
 depends 'cron', '~> 1.7.6'
-#depends 'yum-epel'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
